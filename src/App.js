8103d4a4655afd12e2eb2e064aeaae393aad5f14
thejagithub <thejanaravindu2000@gmail.com>
2023-02-20 13:58:56 +0530

    color change

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ function App() {
     <div className="container">
       <div className="intro-text">
         <div className="intro-lead-in">Welcome To Medi Finder!</div>
-        <div className="intro-heading text-uppercase">It's Nice To Meet You</div>
+        <div className="intro-heading text-uppercase"><span style={{color:"#ffc107"}}>It's</span> Nice To Meet You</div>
         <a className="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#services">Tell Me More</a>
       </div>
     </div>
